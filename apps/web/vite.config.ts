326b69d91dedb8d37509a6669959dd56668eb2f9
Branham Karanja <branham.karanja@outlook.com>
2026-06-29 00:54:58 +0300

    chore(vite): configure Vite server for Cloudflare tunnel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175,
-  }
+    host: true,
+    allowedHosts: [
+      '.branhamkaranja.com',
+    ],
+    port: 5173
+  },
 })
